4248579192b72e4088670899f47bcc97bda317d7
Jailine <jay.santana964@gmail.com>
2023-10-23 17:04:53 -0300

    Aula: Componentes - Criando uma prop

--- Desafio 01/src/components/TituloDinamico.js
+++ Desafio 01/src/components/TituloDinamico.js
@@ -7,7 +7,7 @@ class TituloDinamico extends HTMLElement{
 
         //Base do component
         const componentRoot = document.createElement("h1");
-        componentRoot.textContent = "Felipe";
+        componentRoot.textContent = this.getAttribute("titulo");
         //Estilizar o component
         const style = document.createElement("style");
         style.textContent = `
