3fd0bcdf92bb10c7f382acccad5c822d44f78223
Jailine <jay.santana964@gmail.com>
2023-10-25 15:36:25 -0300

    Aula: TypeScript - Funções multi tipos

--- Funções/funcao.ts
+++ Funções/funcao.ts
@@ -6,7 +6,13 @@ function addOla(nome: string):string{
     return ` Olá ${nome}`;
 }
 
+function fazerLigacao(phone: number | string) : number | string{
+    return phone;
+}
+
 let soma: number = somar(4,7);
 
 console.log(soma);
 console.log(addOla("Ana"));
+console.log(fazerLigacao("alô"));
+console.log(fazerLigacao(5));
