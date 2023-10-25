a9ab3495be59e3007c619a1f37a80e94a180b73e
Jailine <jay.santana964@gmail.com>
2023-10-25 17:27:16 -0300

    Aula: TypeScript - Funções async: tipando

--- Funções/funcao.ts
+++ Funções/funcao.ts
@@ -10,6 +10,10 @@ function fazerLigacao(phone: number | string) : number | string{
     return phone;
 }
 
+async function getDatabase(id: number): Promise<string > {
+    return "felipe";
+}
+
 let soma: number = somar(4,7);
 
 console.log(soma);
