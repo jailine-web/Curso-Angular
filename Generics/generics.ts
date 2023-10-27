b66a694c4dbf459bedf2bacaa1273024a9ae5dae
Jailine <jay.santana964@gmail.com>
2023-10-27 14:06:10 -0300

    Trabalhando com TS node dev- Servidor local que roda ts

--- Generics/generics.ts
+++ Generics/generics.ts
@@ -1,12 +1,13 @@
 //Generics os ... indica que aceita diversos itens 
 
-function concatenaArray(...itens: any[]): any[]{
+function concatenaArray<T>(...itens: T[]): T[]{
     return new Array().concat(...itens);
 }
-const numArray = concatenaArray([1,5], [3]);
-const strArray = concatenaArray(["Ana", "júlia"], ["Teresa"]);
+const numArray = concatenaArray(<number[]>[1,5], [3]);
+const strArray = concatenaArray(<string[]>["Ana", "júlia"], ["Teresa"]);
 
-numArray.push("any x number");
+/*Não aceita mais essa atribuição
+numArray.push("any x number");*/
 
 console.log(numArray);
 console.log(strArray);
--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function imprimirObjetos(pessoa) {
-    console.log(pessoa);
-}
-imprimirObjetos({
-    nome: "Bruce",
-    vulgo: "Batman"
-});
-console.log("Olá mundo!");
