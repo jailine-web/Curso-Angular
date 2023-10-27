287ef8b5b489d31c7d244311163e06a2ef3a69c6
Jailine <jay.santana964@gmail.com>
2023-10-27 15:11:34 -0300

    Aula: decorators - Atributos decorator

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
-function apiVersion(version: any){
-    return (target: any) => {
-        Object.assign(target.prototype, { __version: version, __name: "Felipe" });
+//Atributo decorator
+
+function minLength(length: number){
+    return(classe : any, key: string) =>{
+       //console.log(classe[key])
+       //console.log(key)
+        let _value = classe[key];
+
+        const getter = () => "[Play] "+_value;
+        const setter = (value: string) => {
+            if(value.length < length){
+                throw new Error(`Tamanho manor do que ${length}`);
+            }
+            else{
+                _value = value;
+            }
+        };
+
+        Object.defineProperty(classe, key,{
+            get: getter,
+            set: setter,
+        });
+    };
+}
+
+class Api{
+
+    @minLength(7)
+    name: string;
+
+    constructor(name:string){
+        this.name = name;
     }
 }
 
-@apiVersion("1.5")
-class Api{}
-
-const api = new Api();
-console.log(api.__version);
-console.log(api.__name);
-
+const api = new Api("produtos");
+console.log(api.name);
 
 
 
